429ef6235d509cdc2d5157def9e7ab9898f8199c
amit dash <dbamit@thoughtworks.com>
2018-04-01 20:51:06 +0530

    Enable configurable number of threads for the computation

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,36 @@ use std::thread;
 use std::thread::JoinHandle;
 
 fn main() {
-    estimate_pi();
+    estimate_pi(8_000_000, 8);
 }
 
-pub fn estimate_pi() {
-    let total = 100_000;
+pub fn estimate_pi(total_points: i32, total_threads: i32) {
+    let handles = compute_on_multiple_threads(total_points, total_threads);
 
-    let in_circle_main = get_in_circle_count(total / 2);
+    let mut total_in_circle_points = 0;
 
-    let in_circle_thread1 = compute_on_thread(total / 2)
-        .join()
-        .expect("In circle point count sample space computation on a parallel thread failed!");
+    for handle in handles {
+        total_in_circle_points += handle.join().expect("Thread computation failed!");
+    }
 
-    println!("Value of pi - {}", 4. * ((in_circle_main + in_circle_thread1) as f64) / (total as f64));
+    print_value_of_pi(total_points, total_in_circle_points);
 }
 
-fn compute_on_thread(loop_count: i32) -> JoinHandle<i32> {
+fn print_value_of_pi(total_points: i32, total_in_circle_points: i32) {
+    println!("Value of pi: {}", 4. * ((total_in_circle_points) as f64) / (total_points as f64));
+}
+
+fn compute_on_multiple_threads(total_count: i32, num_of_threads: i32) -> Vec<JoinHandle<i32>> {
+    let mut handles = vec![];
+
+    for _ in 0..num_of_threads {
+        handles.push(compute_on_single_thread(total_count / num_of_threads));
+    }
+
+    handles
+}
+
+fn compute_on_single_thread(loop_count: i32) -> JoinHandle<i32> {
     return thread::spawn(move || {
         get_in_circle_count(loop_count)
     });
@@ -51,6 +65,6 @@ mod tests {
 
     #[bench]
     fn bench_estimate_pi(b: &mut Bencher) {
-        b.iter(|| estimate_pi());
+        b.iter(|| estimate_pi(800_000, 8));
     }
 }
