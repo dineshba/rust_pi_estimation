d5bae0ec6a26944ae146d99bff36f1571fced140
Dinesh <dineshudt17@gmail.com>
2018-04-15 21:51:53 +0530

    [Dinesh] Simply cloning the transmitter

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,18 @@ use std::thread;
 use std::time::Instant;
 use std::sync::mpsc;
 use std::sync::mpsc::{Sender, Receiver};
-use std::sync::{Mutex, Arc};
 
 fn main() {
     let now = Instant::now();
-    estimate_pi(8_000_000, 8);
+    estimate_pi(80_000_000, 8);
     let time_taken = now.elapsed();
     println!("Time taken in seconds: {}.{}", time_taken.as_secs(), time_taken.subsec_nanos());
 }
 
 pub fn estimate_pi(total_points: i32, total_threads: i32) {
     let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
-    let arc_tx = Arc::new(Mutex::new(tx));
 
-    compute_on_multiple_threads(total_points, total_threads, arc_tx);
+    compute_on_multiple_threads(total_points, total_threads, tx);
 
     let mut total_in_circle_points = 0;
     for i in rx {
@@ -35,13 +33,12 @@ fn print_value_of_pi(total_points: i32, total_in_circle_points: i32) {
     println!("Value of pi: {}", 4. * ((total_in_circle_points) as f64) / (total_points as f64));
 }
 
-fn compute_on_multiple_threads(total_count: i32, num_of_threads: i32, arc_tx: Arc<Mutex<Sender<i32>>>) {
+fn compute_on_multiple_threads(total_count: i32, num_of_threads: i32, tx: Sender<i32>) {
     for _i in 0..num_of_threads {
-        let arc_tx = Arc::clone(&arc_tx);
+        let new_tx = mpsc::Sender::clone(&tx);
         thread::spawn(move || {
             let in_circle_count = get_in_circle_count(total_count / num_of_threads);
-            let tx = arc_tx.lock().unwrap();
-            tx.send(in_circle_count).unwrap();
+            new_tx.send(in_circle_count).unwrap();
         });
     }
 }
