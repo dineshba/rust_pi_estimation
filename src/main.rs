076a41cfeb7e7afccd099c8c23e650d13148d428
Dinesh B <dineshudt17@gmail.com>
2018-04-15 10:37:11 +0530

    [Dinesh] Using ARC and channels

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ extern crate rand;
 
 use rand::Rng;
 use std::thread;
-use std::thread::JoinHandle;
 use std::time::Instant;
+use std::sync::mpsc;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::{Mutex, Arc};
 
 fn main() {
     let now = Instant::now();
@@ -16,13 +18,15 @@ fn main() {
 }
 
 pub fn estimate_pi(total_points: i32, total_threads: i32) {
-    let handles = compute_on_multiple_threads(total_points, total_threads);
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+    let arc_tx = Arc::new(Mutex::new(tx));
+
+    compute_on_multiple_threads(total_points, total_threads, arc_tx);
 
     let mut total_in_circle_points = 0;
-
-    for handle in handles {
-        total_in_circle_points += handle.join().expect("Thread computation failed!");
-    }
+    for i in rx {
+        total_in_circle_points += i;
+    };
 
     print_value_of_pi(total_points, total_in_circle_points);
 }
@@ -31,14 +35,15 @@ fn print_value_of_pi(total_points: i32, total_in_circle_points: i32) {
     println!("Value of pi: {}", 4. * ((total_in_circle_points) as f64) / (total_points as f64));
 }
 
-fn compute_on_multiple_threads(total_count: i32, num_of_threads: i32) -> Vec<JoinHandle<i32>> {
-    (0..num_of_threads).map(|_| compute_on_single_thread(total_count / num_of_threads)).collect()
-}
-
-fn compute_on_single_thread(loop_count: i32) -> JoinHandle<i32> {
-    return thread::spawn(move || {
-        get_in_circle_count(loop_count)
-    });
+fn compute_on_multiple_threads(total_count: i32, num_of_threads: i32, arc_tx: Arc<Mutex<Sender<i32>>>) {
+    for _i in 0..num_of_threads {
+        let arc_tx = Arc::clone(&arc_tx);
+        thread::spawn(move || {
+            let in_circle_count = get_in_circle_count(total_count / num_of_threads);
+            let tx = arc_tx.lock().unwrap();
+            tx.send(in_circle_count).unwrap();
+        });
+    }
 }
 
 fn get_in_circle_count(loop_count: i32) -> i32 {
